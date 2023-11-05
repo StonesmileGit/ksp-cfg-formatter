cd1f0350b6314336ffd63416e1895d828f34c20b
Stonesmile <62522391+StonesmileGit@users.noreply.github.com>
2023-11-05 15:04:13 +0100

    Add xtask compilation support for linux

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -3,7 +3,7 @@ use std::{
     path::{Path, PathBuf},
 };
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use xshell::{cmd, Shell};
 
 #[derive(Parser)]
@@ -13,6 +13,15 @@ struct Args {
     ci: bool,
     #[arg(long, help = "Install the generated .vsix")]
     install: bool,
+    #[arg(long, help = "which platform to compile against")]
+    platform: Platform,
+}
+
+#[derive(ValueEnum, Clone)]
+enum Platform {
+    All,
+    Windows,
+    Unix,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -20,21 +29,57 @@ fn main() -> anyhow::Result<()> {
     let sh = &Shell::new()?;
     let dir = project_root();
     sh.change_dir(dir);
-    cmd!(sh, "cargo build -p lsp-rs --release").run()?;
+
+    let targets = get_targets(&args);
     sh.create_dir("./lsp-extension/server")?;
-    sh.copy_file(
-        "./target/release/lsp-rs.exe",
-        "./lsp-extension/server/lsp-rs.exe",
-    )?;
-    sh.change_dir("./lsp-extension");
-    if args.ci {
-        cmd!(sh, "cmd.exe /c npm ci").run()?;
+    for target in targets {
+        cmd!(sh, "cargo build -p lsp-rs --release --target {target}").run()?;
+        if target.contains("windows") {
+            sh.copy_file(
+                format!("./target/{target}/release/lsp-rs.exe"),
+                "./lsp-extension/server/ksp-cfg-lsp.exe",
+            )?;
+        } else {
+            sh.copy_file(
+                format!("./target/{target}/release/lsp-rs"),
+                "./lsp-extension/server/ksp-cfg-lsp",
+            )?;
+        }
     }
-    cmd!(sh, "cmd.exe /c npm run package").run()?;
-    if args.install {
-        cmd!(sh, "cmd.exe /c code --install-extension ksp-cfg-lsp.vsix").run()?;
+    sh.change_dir("./lsp-extension");
+    if cfg!(target_family = "unix") {
+        if args.ci {
+            cmd!(sh, "npm ci").run()?;
+        }
+        cmd!(sh, "npm run package").run()?;
+        if args.install {
+            cmd!(sh, "code --install-extension ksp-cfg-lsp.vsix").run()?;
+        }
+    } else {
+        if args.ci {
+            cmd!(sh, "cmd.exe /c npm ci").run()?;
+        }
+        cmd!(sh, "cmd.exe /c npm run package").run()?;
+        if args.install {
+            cmd!(sh, "cmd.exe /c code --install-extension ksp-cfg-lsp.vsix").run()?;
+        }
     }
     Ok(())
+}
+
+fn get_targets(args: &Args) -> Vec<&str> {
+    let mut targets = vec![];
+    if matches!(args.platform, Platform::Unix | Platform::All) {
+        targets.push("x86_64-unknown-linux-gnu");
+    }
+    if matches!(args.platform, Platform::Windows | Platform::All) {
+        if cfg!(target_family = "unix") {
+            targets.push("x86_64-pc-windows-gnu");
+        } else {
+            targets.push("x86_64-pc-windows-msvc");
+        }
+    }
+    targets
 }
 
 fn project_root() -> PathBuf {
